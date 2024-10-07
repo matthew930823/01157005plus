9a70301e81d3813b1cecb0ee4d67cb70a86aa44f
Chien Yu <leoyu93.01.04@gmail.com>
2024-10-07 17:08:10 +0800

    Initial Commit

---
+++ 01157005plus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01157005plus
+//
+//  Created by user11 on 2024/10/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01157005plus/_1157005plusApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1157005plusApp.swift
+//  01157005plus
+//
+//  Created by user11 on 2024/10/7.
+//
+
+import SwiftUI
+
+@main
+struct _1157005plusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
